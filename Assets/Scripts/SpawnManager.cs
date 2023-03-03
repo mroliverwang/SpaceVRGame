a88794bd043d89a6cf6ae665018c2db691753876
Oliver Wang <mroliver_w@outlook.com>
2023-03-03 16:37:55 +0000

    spawn new balls with button

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject ball;
+    private InputDevice targetDevice;
     // Start is called before the first frame update
     void Start()
     {
-        
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool buttondown);
+        if (buttondown)
         {
             Instantiate(ball, ball.transform.position, ball.transform.rotation);
 
