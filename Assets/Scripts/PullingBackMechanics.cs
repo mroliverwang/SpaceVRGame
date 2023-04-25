e8a9e27627a21cb349f2e241e35539a3b7bdf8b4
Ranranrenee <renee.rr.wang@gmail.com>
2023-04-25 21:23:57 +0100

    Bomb Asserts

--- Assets/Scripts/PullingBackMechanics.cs
+++ Assets/Scripts/PullingBackMechanics.cs
@@ -7,7 +7,9 @@ public class PullingBackMechanics : MonoBehaviour
 {
     private InputDevice targetDevice;
     private Rigidbody rb;
-    public GameObject player;
+    [SerializeField]
+    private GameObject player;
+
     private Vector3 newVel;
     private Vector3 curVel;
     private float pullingSpeed;
@@ -16,6 +18,8 @@ public class PullingBackMechanics : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("OVRPlayerController");
+
         List<InputDevice> devices = new List<InputDevice>();
         InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
         InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
